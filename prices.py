55306617f3ab12ff0084ef860bf53bfb0b432cbf
GeorgeManakanatas <manakanatas@gmail.com>
2021-09-02 16:19:45 +0200

    price tracking basic

--- prices.py
+++ prices.py
@@ -16,3 +16,20 @@ interface = PostgresqlInterface()
 if my_config.config_values['initialize_db']:
     logger.info('Initializing database')
     interface.init_postgresql()
+
+
+if my_config.config_values['check_product_prices']:
+    logger.info('Getting product prices')
+    # read configuration file
+    with open('config/tracked_products_list.json', 'r') as prods:
+        products_config = json.load(prods)
+    #
+    for product in products_config['items_list']:
+        time.sleep(random.randint(10,60))
+        page = requests.get(product['url'],headers={"User-Agent":"Defined"})
+        logger.info(str(product['price_class']))
+        logger.info(str(page))
+        product_soup = BeautifulSoup(page.content, 'html.parser')
+        price = product_soup.find('span', class_=product['price_class'])
+        logger.info('The price is: '+str(price))
+            
